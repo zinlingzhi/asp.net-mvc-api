5a56f5f3022a0809e4cf93a66bd7d4d4fcee5d09
decaf-programmer <decaf.programmer@gmail.com>
2021-03-16 11:32:34 +0800

    Create customized model and controller and views

---
+++ ASPDemoApplication/MVCDemo/Controllers/PeopleController.cs
@@ -0,0 +1,26 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+
+            people.Add(new PersonModel { FirstName = "tim", LastName = "Corey", Age = 38 });
+            return View(people);
+        }
+    }
+}
